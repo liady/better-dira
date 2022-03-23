9a05a0d3a0143c0346b8f2a38e151e213a98ad4b
Liad Yosef <liad@duda.co>
2022-03-24 00:22:28 +0200

    add per city

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
     );
     setFetching(false);
     setRefreshed(true);
-  }, [rowData, fetching]);
+  }, [fetching, rowData, setRefreshed]);
 
   window.fetchAll = fetchAll;
 
